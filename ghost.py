7f7f9847e148271d8d3ec97b0b29df95648f437c
Adrian Pegler <adrian.pegler@mail.de>
2023-06-21 08:07:14 +0200

    First function

---
+++ ghost.py
@@ -0,0 +1,20 @@
+import cyberpi
+import time
+import math, random
+
+def pi_print(message):
+    cyberpi.display.show_label(message, 16, int(8*0 + 5), int(17 * 0))
+
+def run():
+    pi_print("Hello World!")
+
+
+def main():
+    try:
+        run()
+    except Exception as e:
+        cyberpi.display.show_label("Exeption on Mbot 2", 16, int(8 * 0 + 5), int(17 * 0))
+        cyberpi.display.show_label(e, 16, int(8 * 0 + 5), int(17 * 1))
+        raise
+
+main()
