061269afce05c4151da8e5c730cf1c17ff3c6f5e
Adrian Pegler <adrian.pegler@mail.de>
2023-08-28 12:14:40 +0200

    Hoffentlich Konfliktbehaftet

--- ghost.py
+++ ghost.py
@@ -2,13 +2,19 @@ import cyberpi
 import time
 import math, random
 
+#Sendet eine Nachricht an den CyberPi-Display
 def pi_print(message):
     cyberpi.display.show_label(message, 16, int(8*0 + 5), int(17 * 0))
 
+#Hier kommt alles rein, was der Pi / mBot ausführen soll
 def run():
     pi_print("Hello World!")
 
+#Sinnlose Testfunktion um einen Konflikt zu erzeugen
+def my_sensless_fun():
+    pi_print("Woolooloo!!")
 
+#Vom Pi vorgegebene Main-Funktion
 def main():
     try:
         run()
@@ -16,7 +22,4 @@ def main():
         cyberpi.display.show_label("Exeption on Mbot 2", 16, int(8 * 0 + 5), int(17 * 0))
         cyberpi.display.show_label(e, 16, int(8 * 0 + 5), int(17 * 1))
         raise
-    #Ich mach nur einen Commit
-    #Und jetzt einen Push
-    #Hallo
 main()
