b89fdb99b2c88fb913972bf6bb5a3220974c814e
emily paap <emily.leonie.paap@pus-eck.org>
2023-09-11 11:59:47 +0200

    Emily

--- ghost.py
+++ ghost.py
@@ -2,6 +2,8 @@ import cyberpi
 import time
 import math, random
 
+#Das ist mein branch. emily
+
 #Sendet eine Nachricht an den CyberPi-Display
 def pi_print(message):
     cyberpi.display.show_label(message, 16, int(8*0 + 5), int(17 * 0))
