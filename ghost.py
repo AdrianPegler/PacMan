0a472d4874d326550492abc0fa6059a54d7c59ef
John Doe <johndoe@example.com>
2023-09-11 12:15:34 +0200

    kein plan

--- ghost.py
+++ ghost.py
@@ -2,6 +2,7 @@ import cyberpi
 import time
 import math, random
 
+#ab hier arbeite ich alleine
 #Sendet eine Nachricht an den CyberPi-Display
 def pi_print(message):
     cyberpi.display.show_label(message, 16, int(8*0 + 5), int(17 * 0))
@@ -24,4 +25,5 @@ def main():
         cyberpi.display.show_label("Exeption on Mbot 2", 16, int(8 * 0 + 5), int(17 * 0))
         cyberpi.display.show_label(e, 16, int(8 * 0 + 5), int(17 * 1))
         raise
+
 main()
