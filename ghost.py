b343a9076478162a0c226a0b3180f5f9924d0c77
Berzan Göz <berzan.goez@pus-eck.org>
2023-09-11 11:49:38 +0200

    hallo

--- ghost.py
+++ ghost.py
@@ -2,21 +2,13 @@ import cyberpi
 import time
 import math, random
 
-#Sendet eine Nachricht an den CyberPi-Display
 def pi_print(message):
     cyberpi.display.show_label(message, 16, int(8*0 + 5), int(17 * 0))
 
-#Hier kommt alles rein, was der Pi / mBot ausführen soll
 def run():
     pi_print("Hello World!")
-def hello():
-    pi_print("Hello from the other side")
-    
-#Sinnlose Testfunktion um einen Konflikt zu erzeugen
-def my_sensless_fun():
-    pi_print("Woolooloo!!")
 
-#Vom Pi vorgegebene Main-Funktion
+
 def main():
     try:
         run()
@@ -24,4 +16,5 @@ def main():
         cyberpi.display.show_label("Exeption on Mbot 2", 16, int(8 * 0 + 5), int(17 * 0))
         cyberpi.display.show_label(e, 16, int(8 * 0 + 5), int(17 * 1))
         raise
+#hallo
 main()
